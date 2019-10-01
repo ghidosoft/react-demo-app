f5ddaa2c683228844734e0d81fb9d63e7763d13c
Andrea Ghidini <andrea.ghidini@looptribe.com>
2019-10-01 15:23:41 +0200

    redux-saga

--- src/store.js
+++ src/store.js
@@ -1,7 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
+import sagas from './sagas';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(sagas);
 
 export default store;
